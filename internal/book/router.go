533af5dc82925bf006e29669f98d888bfb8ee373
swpark6 <sewon1234@naver.com>
2020-05-31 17:03:36 +0900

    Done: - Add endpoint /api/books/ Todo: - Redircet to 405 page - Connect to database - Implement endpoint /api/books/{id}

--- internal/book/router.go
+++ internal/book/router.go
@@ -7,8 +7,13 @@ import (
 
 // Router default
 func Router(mux *http.ServeMux) {
-	mux.HandleFunc("/book/", func(responseWriter http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc("/api/books", func(responseWriter http.ResponseWriter, request *http.Request) {
 		io.WriteString(responseWriter, "Hello Book from a HandleFunc!\n")
 		io.WriteString(responseWriter, request.URL.Path)
+		if request.Method != http.MethodGet {
+			// TODO: Redirect 405
+		}
+		// TODO: Get book list from database
 	})
+	// TODO: /api/books/{id} --> No id --> Redirect to /api/books
 }
